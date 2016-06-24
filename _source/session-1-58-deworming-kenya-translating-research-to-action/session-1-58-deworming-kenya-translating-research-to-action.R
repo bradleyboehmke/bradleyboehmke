57a33b1559e7f1811ccdc8558eb6cc4d0be2dcae
Brad Boehmke <bradleyboehmke@gmail.com>
2016-06-23 21:02:50 -0400

    first harvard session reading assignment

---
+++ _source/session-1-58-deworming-kenya-translating-research-to-action/session-1-58-deworming-kenya-translating-research-to-action.R
@@ -0,0 +1,4 @@
+# This R file accomanies the .Rmd blog post
+# _source/session-1-58-deworming-kenya-translating-research-to-action/2016-06-23-session-1-58-deworming-kenya-translating-research-to-action.Rmd
+
+
