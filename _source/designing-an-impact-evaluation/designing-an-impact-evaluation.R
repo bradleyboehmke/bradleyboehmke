dd28d8c6e6f7ea19f902ef5b7995d7ed39016505
Brad Boehmke <bradleyboehmke@gmail.com>
2016-06-29 15:09:17 -0400

    decision making IE notes

---
+++ _source/designing-an-impact-evaluation/designing-an-impact-evaluation.R
@@ -0,0 +1,4 @@
+# This R file accomanies the .Rmd blog post
+# _source/designing-an-impact-evaluation/2016-06-29-designing-an-impact-evaluation.Rmd
+
+
---
+++ _source/lean-decision-making-impact-evaluations/lean-decision-making-impact-evaluations.R
@@ -0,0 +1,4 @@
+# This R file accomanies the .Rmd blog post
+# _source/lean-decision-making-impact-evaluations/2016-06-29-lean-decision-making-impact-evaluations.Rmd
+
+
