32bb7b84b8d9c52b93519648cbec5a943df0636c
Brad Boehmke <bradleyboehmke@gmail.com>
2017-01-01 17:16:50 -0500

    Data wrangling with r post
    Updated post about the publication of this book

--- _source/data-wrangling-with-r/data-wrangling-with-r.R
+++ _source/data-wrangling-with-r/data-wrangling-with-r.R
@@ -1,4 +1,3 @@
 # This R file accomanies the .Rmd blog post
-# _source/data-wrangling-with-r/2016-03-13-data-wrangling-with-r.Rmd
-
+# _source/data-wrangling-with-r/2017-01-01-data-wrangling-with-r.Rmd
 
